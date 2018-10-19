d762d38220c407800c79777b7a46f25619d9301c
joyhuangg <joyhuangg@gmail.com>
2018-10-19 14:50:26 -0400

    small bug fix

--- app/models/stamp_card.rb
+++ app/models/stamp_card.rb
@@ -1,5 +1,5 @@
 class StampCard < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, optional: true
   belongs_to :deal
 
   validates :current_points, numericality: {less_than_or_equal_to: 6}
