ee3daa3d3ab74bf6cfe57eb1a89c3c4029483fa4
joyhuangg <joyhuangg@gmail.com>
2018-10-22 15:19:30 -0400

    Added store access for stampcards

--- app/serializers/stamp_card_serializer.rb
+++ app/serializers/stamp_card_serializer.rb
@@ -1,9 +1,13 @@
 class StampCardSerializer < ActiveModel::Serializer
-  attributes :id, :customer_id, :deal_id, :current_points, :store_id, :deal
+  attributes :id, :customer_id, :deal_id, :current_points, :store_id, :deal, :store
 
   def store_id
   # all_cards = {}
     object.deal.store.id
+  end
+
+  def store
+    object.deal.store
   end
 
 end
